ecd4ee0e8592a20cdb7742a6cb351b8d932b7437
Sean Wu <swu145@aucklanduni.ac.nz>
2017-06-22 22:43:27 +1200

    initial commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+void sh_loop();
+
+
+
+void sh_loop(){
+	
+}
+
+int main(int argc, char **argv){
+	printf("Hello World\n");
+	sh_loop();
+	return EXIT_SUCCESS;
+}
